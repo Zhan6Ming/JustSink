76196eb343d1f1d4af9fbb8a79b55bce5c5c368c
ZhanMing <nanbeiluduo@foxmail.com>
2026-07-04 14:56:10 +0800

    fix: 移除多余的泛型事件监听器注册，ModCapabilities已有@EventBusSubscriber注解

--- src/main/java/com/github/zhan6ming/just_sink/JustSink.java
+++ src/main/java/com/github/zhan6ming/just_sink/JustSink.java
@@ -22,7 +22,6 @@ public class JustSink {
     public JustSink() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         modEventBus.addListener(this::commonSetup);
-        modEventBus.addListener(ModCapabilities::registerCapabilities);
         ModRegistries.register(modEventBus);
         MinecraftForge.EVENT_BUS.register(this);
     }
